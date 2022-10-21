933732dd0eb5b810f38508392ef003ef3267c3b9
DS-Daniel <daniel.sciarra@heig-vd.ch>
2022-10-21 17:42:30 +0200

    set types to custom table components

--- frontend/src/components/common/table/customTableComponents.ts
+++ frontend/src/components/common/table/customTableComponents.ts
@@ -1,15 +1,19 @@
-import TableContainer from '@mui/material/TableContainer';
-import TableCell from '@mui/material/TableCell';
-import TableRow from '@mui/material/TableRow';
-import TableHead from '@mui/material/TableHead';
+import TableContainer, {
+	TableContainerProps,
+} from '@mui/material/TableContainer';
+import TableCell, { TableCellProps } from '@mui/material/TableCell';
+import TableRow, { TableRowProps } from '@mui/material/TableRow';
+import TableHead, { TableHeadProps } from '@mui/material/TableHead';
 import { styled } from '@mui/material/styles';
+
+interface StyledTableCellProps extends TableCellProps {
+	borderR: boolean;
+}
 
 // custom TableCell component
 export const StyledTableCell = styled(TableCell, {
 	shouldForwardProp: (prop) => prop !== 'borderR',
-})<{
-	borderR: boolean;
-}>(({ borderR, theme }) => ({
+})<StyledTableCellProps>(({ borderR, theme }) => ({
 	// [`&.${tableCellClasses.head}`]: {
 	// 	color: theme.palette.common.white,
 	// 	borderRight: '1px solid',
@@ -22,25 +26,25 @@ export const StyledTableCell = styled(TableCell, {
 }));
 
 // custom TableRow component
-export const StyledTableRow = styled(TableRow)(({ theme }) => ({
+export const StyledTableRow = styled(TableRow)<TableRowProps>(({ theme }) => ({
 	'&:nth-of-type(even)': {
 		backgroundColor: theme.palette.action.hover,
 	},
-	// hide last border
-	// '&:last-child td, &:last-child th': {
-	//   border: 0,
-	// },
 }));
 
 // custom TableContainer component
-export const StyledTableContainer = styled(TableContainer)(({ theme }) => ({
-	border: `1px solid ${theme.palette.divider}`,
-	borderRadius: `${theme.shape.borderRadius * 2}px ${
-		theme.shape.borderRadius * 2
-	}px 0 0`,
-}));
+export const StyledTableContainer = styled(TableContainer)<TableContainerProps>(
+	({ theme }) => ({
+		border: `1px solid ${theme.palette.divider}`,
+		borderRadius: `${theme.shape.borderRadius * 2}px ${
+			theme.shape.borderRadius * 2
+		}px 0 0`,
+	}),
+);
 
 // custom TableHead component
-export const OrangeTableHead = styled(TableHead)(({ theme }) => ({
-	backgroundColor: theme.palette.primary.light,
-}));
+export const OrangeTableHead = styled(TableHead)<TableHeadProps>(
+	({ theme }) => ({
+		backgroundColor: theme.palette.primary.light,
+	}),
+);
