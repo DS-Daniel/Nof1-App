99901e2c5f52fcab721fc0ad13be9d08da9eb3a8
DS-Daniel <daniel.sciarra@heig-vd.ch>
2022-12-19 11:04:46 +0100

    fix sticky footer

--- frontend/src/components/layout/Page.tsx
+++ frontend/src/components/layout/Page.tsx
@@ -1,4 +1,5 @@
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import { ReactNode } from 'react';
 import NavBar from './NavBar';
 import Footer from './Footer';
@@ -12,18 +13,19 @@ type PageProps = {
  */
 export default function Page({ children }: PageProps) {
 	return (
-		<>
+		<Box minHeight={'100vh'} display="flex" flexDirection="column">
 			<NavBar />
 			<Container
 				component="main"
 				maxWidth="lg"
 				sx={{
 					my: 4,
+					flex: 1,
 				}}
 			>
 				{children}
 			</Container>
 			<Footer />
-		</>
+		</Box>
 	);
 }
